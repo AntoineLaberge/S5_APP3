ff3e96a64f15514d575239fc82c824fbce1f85e2
Jonathan <degj2706@usherbrooke.ca>
2021-10-04 20:00:39 -0400

    beethoven works!

--- problematique.py
+++ problematique.py
@@ -4,53 +4,74 @@ from scipy.io import wavfile
 
 filePath = "./note_guitare_LAd.wav"
 
-def get_k_index(note):
+
+def get_note_freq(note):
     mapping = {
-        "DO" : -10,
+        "DO": 261.6,
+        "DO#": 277.2,
+        "RE": 293.7,
+        "RE#": 311.1,
+        "MI": 329.6,
+        "FA": 349.2,
+        "FA#": 370,
+        "SOL": 392,
+        "SOL#": 415.3,
+        "LA": 440,
+        "LA#": 466.2,
+        "SI": 493.9
+    }
+    return mapping[note]
+
+
+def get_k_index_from_note(note):
+    mapping = {
+        "DO": -10,
         "DO#": -9,
-        "RE" : -8,
+        "RE": -8,
         "RE#": -7,
-        "MI" : -6,
-        "FA" : -5,
+        "MI": -6,
+        "FA": -5,
         "FA#": -4,
         "SOL": -3,
-        "SOL#":-2,
-        "LA" : -1,
+        "SOL#": -2,
+        "LA": -1,
         "LA#": 0,
-        "SI" : 1
+        "SI": 1
     }
     return mapping[note]
 
 
 def synthetize_song(X_m, m_indexes, envelope, nb_ech, notes):
     final_sound = []
-    nb_notes_kept = int(len(envelope)/len(notes))
+    nb_notes_kept = int(len(envelope) / len(notes))
     for note in notes:
         synthetized_note = synthetize_signal(X_m, m_indexes, envelope, nb_ech, note)
-        final_sound = np.concatenate((final_sound,synthetized_note[:nb_notes_kept]))
+        final_sound = np.concatenate((final_sound, synthetized_note[:nb_notes_kept]))
     return final_sound
 
 
 def synthetize_signal(X_m, m_indexes, envelope, nb_ech, note):
     if note == "SILENCE":
         return np.zeros(len(envelope))
-    k_index = get_k_index(note)
-    factor = 2**(k_index/12)
+
+    k_index = get_k_index_from_note(note)
+    factor = 2 ** (k_index / 12)
     phases = np.angle(X_m)
     magnitudes = np.abs(X_m)
 
     ws = [2 * np.pi * m * factor / nb_ech for m in m_indexes]
     ws_normalized = [w - (2 * np.pi) if w > np.pi else w for w in ws]
 
-    ws_normalized.sort()
     sum_sines = np.zeros(len(envelope))
 
     for n in range(len(envelope)):
         sum_sines[n] = np.sum(magnitudes * np.sin(np.multiply(n, ws_normalized) + phases))
 
-    synthetized_signal = sum_sines * envelope
+    synthetized_x_n = sum_sines * envelope
 
-    return synthetized_signal
+    synthetized_x_n_corrected = [magnitude/np.amax(synthetized_x_n) for magnitude in synthetized_x_n]
+
+    return synthetized_x_n_corrected
 
 
 def get_tfd(x_n):
@@ -62,17 +83,62 @@ def get_tfd(x_n):
     return X_m, X_phase, X_magnitude
 
 
+def get_m_fund_freq_indexes(X_m, fund_freq):
+    m_fund_freq_indexes = []
+
+    for i in range(len(X_m)):
+        if i != 0 and i % fund_freq == 0:
+            m_fund_freq_indexes.append(i)
+
+    return m_fund_freq_indexes
+
+
+def is_principal_sinusoid(current, prev, next, threshold):
+    # check if current > threshold and current > neighbours
+    if current > threshold and current > prev and current > next:
+        return True
+    return False
+
+
+def find_n_largest(array, n):
+    indexes = np.argpartition(array, -n)[-n:]
+    return np.sort(indexes)
+
+
+def get_principal_sinusoids_v2(X_m, note, fe):
+    #Transform fund_freq to fund_m using relation f/fe = m/n
+    fund_freq = int(get_note_freq(note))
+    fund_m = int((fund_freq/fe)*len(X_m))
+
+    # Get indexes for X_ms that are a multiple of fundamental m.
+    m_fund_freq_indexes = get_m_fund_freq_indexes(X_m, fund_m)
+
+    # Retrieve principal X_ms
+    principal_X_ms = [X_m[m] for m in m_fund_freq_indexes]
+
+    # Get 32 highest peaks
+    principal_X_ms_magnitudes = np.abs(principal_X_ms)
+    # We must trace back the original index (0->466, 1->466*2, etc) so (m+1) * fund_freq
+    # Because indexes that we found with find_n_largest gives indexes between 0 and len(array)
+    principal_X_ms_indexes = [(m + 1) * fund_m for m in find_n_largest(principal_X_ms_magnitudes, 32)]
+
+    principal_X_ms = [X_m[m] for m in principal_X_ms_indexes]
+
+    return principal_X_ms_indexes, principal_X_ms
+
+
 def get_principal_sinusoids(X_m):
     # Fin max amplitude from X_m
     X_max = np.amax(abs(X_m))
     m_indexes = []
-    treshold = X_max * 0.1
+    threshold = X_max * 0.1
 
     X_magnitudes = np.abs(X_m)
 
-    for i in range(len(X_magnitudes)):
-
-        if X_magnitudes[i] > treshold and X_magnitudes[i] > X_magnitudes[i-1] and X_magnitudes[i] > X_magnitudes[i+1]:
+    # For each amp, check if its > threshold and > neighbours
+    for i in range(len(X_magnitudes) - 1):
+        if X_magnitudes[i] > threshold and X_magnitudes[i] > X_magnitudes[i - 1] and X_magnitudes[i] > X_magnitudes[
+            i + 1]:
             m_indexes.append(i)
 
     principal_X_m = [X_m[i] for i in m_indexes]
@@ -206,27 +272,30 @@ def analyzeWav(file):
     X_m, X_phase, X_magnitude = get_tfd(x_n_hanning)
 
     # Get max 32 best sinusoids
-    m_indexes, principal_X_m = get_principal_sinusoids(X_m)
+    # m_indexes, principal_X_m = get_principal_sinusoids(X_m)
 
-    #Synthetize signal by adding all best sinusoids and multiplying by the enveloppe
-    synthetized_signal = synthetize_signal(principal_X_m, m_indexes, envelope, len(x_n), "LA#")
+    m_indexes_v2, principal_X_m_v2 = get_principal_sinusoids_v2(X_m, "LA#", fe)
+
+    # Synthetize signal by adding all best sinusoids and multiplying by the enveloppe
+    synthetized_signal = synthetize_signal(principal_X_m_v2, m_indexes_v2, envelope, len(x_n), "LA#")
     plt.plot(synthetized_signal)
     plt.title("Synthetized signal x[n]")
     plt.xlabel("n")
     plt.ylabel("x[n]")
     plt.show()
-    wavfile.write("note_guitare_LAd_output.wav", fe, synthetized_signal)
+    wavfile.write("note_guitare_LAd_output.wav", fe, np.array(synthetized_signal, dtype=np.float32))
 
-    #Beethoven 5th symphony
-    #SOL SOL SOL MI bémol (silence) FA FA FA RE.
+    # Beethoven 5th symphony
+    # SOL SOL SOL MI bémol (silence) FA FA FA RE.
     notes = ["SOL", "SOL", "SOL", "RE#", "SILENCE", "FA", "FA", "FA", "RE"]
-    synthetized_song = synthetize_song(principal_X_m, m_indexes, envelope, len(x_n), notes)
+    synthetized_song = synthetize_song(principal_X_m_v2, m_indexes_v2, envelope, len(x_n), notes)
     plt.plot(synthetized_song)
     plt.title("Synthetized song x[n]")
     plt.xlabel("n")
     plt.ylabel("x[n]")
     plt.show()
     wavfile.write("beethoven.wav", fe, synthetized_song)
+
 
 if __name__ == "__main__":
     analyzeWav(filePath)
